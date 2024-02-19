0160bdb79a9d726047ceaa257e93c961bbc687d3
Rodrigo do N Silva <rodrigo.nascim.silva@gmail.com>
2024-02-18 22:53:05 -0300

    Configuração de imports com sucrase e exec com nodemon

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const routes = require('./router')
+import express from 'express';
+import routes from './router';
 
 class App {
 
@@ -18,4 +18,4 @@ class App {
     }
 }
 
-module.exports = new App().server;
+export default new App().server;
--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ ok: true });
+    return res.json({ ok: false });
 });
 
-module.exports = routes;
+export default routes;
 
